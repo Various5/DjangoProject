5eeeb4079bee875c2e057a511a3232400a40accc
varous555 <varous555@gmail.com>
2025-04-04 11:36:32 +0200

    --

--- pcrmgmtAPP/context_processors.py
+++ pcrmgmtAPP/context_processors.py
@@ -4,4 +4,4 @@ def current_theme(request):
     if request.user.is_authenticated:
         profile, created = UserProfile.objects.get_or_create(user=request.user)
         return {'current_theme': profile.theme}
-    return {'current_theme': 'light'}
+    return {'current_theme': 'dark'}
